cdc0662c25556f4bd36bcb5bae70e7e58d634245
Valeriy Yumm <culwork@yandex.ru>
2018-02-06 20:29:04 +0500

    Root files

---
+++ .bottom.menu.php
@@ -0,0 +1,48 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Новости",
+		"news/",
+		Array(),
+		Array(),
+		""
+	),
+
+	Array(
+		"Как купить",
+		"about/howto/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+
+	Array(
+		"Доставка",
+		"about/delivery/",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"О нас",
+		"about/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Гарантии",
+		"about/guaranty/",
+		Array(),
+		Array(),
+		""
+	),
+	Array(
+		"Контакты",
+		"about/contacts/",
+		Array(),
+		Array(),
+		""
+	),
+);
+?>
